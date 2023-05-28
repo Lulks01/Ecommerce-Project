0134252658454d85959b3e25f54c51e7038f68d1
Marcos Tokashiki <101606887+Tokashiki-DEV@users.noreply.github.com>
2023-05-28 17:13:41 -0400

    start do projeto

---
+++ ecommerce/src/footer.js
@@ -0,0 +1,17 @@
+import './footer.css';
+
+
+export function Footer() {
+    return (
+        <div className='Footer'>
+            <div className='containerFooter'>
+                <a href="https://www.youtube.com/watch?v=HEXWRTEbj1I&pp=ygUMd2hhdCBpcyBsb3Zl">
+                    <h1 id='renovebuton'>RenoveSaude</h1>
+                </a>
+                <p>Confiança e credibilidade é aqui</p>
+                <div> </div>
+            </div>
+        </div>
+    );
+};
+
